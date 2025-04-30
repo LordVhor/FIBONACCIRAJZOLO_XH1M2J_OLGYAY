74400192c1e2e3c3c380b9c52ff0c065c77c5527
LordVhor <olgyaymate2004@gmail.com>
2025-04-30 16:00:25 +0200

    Fetch restructured

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -1,28 +1,28 @@
+let fibonacci;
 
-
-
-document.getElementById("sendButton").addEventListener('click', function() 
+function GetFibonacci(iteration)
 {
-
-    let input = document.getElementById('fibonacciInput').value;
-    
     fetch('https://localhost:44342/api/Fibonacci', 
-    {
-        method: 'POST',
-        headers: 
         {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(Number(input))
-    })
-    .then(response => response.json())
-    .then(data => 
-    {
-        fibonacci=data;
-        console.log(fibonacci.sequenceArray); 
-        
-         
-    })
-    .catch(error => console.error('Hiba történt:', error));
-});
+            method: 'POST',
+            headers: 
+            {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(Number(iteration))
+        })
+        .then(response => response.json())
+        .then(data => 
+        {
+            fibonacci=data;
+            console.log(fibonacci.sequenceArray); 
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+
+
+document.getElementById("sendButton").addEventListener("click", GetFibonacci(document.getElementById('fibonacciInput').value));
+
 
