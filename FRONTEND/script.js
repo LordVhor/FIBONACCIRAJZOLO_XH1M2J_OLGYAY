19393aeee700b19112587c303d0cd8dbe1451712
LordVhor <olgyaymate2004@gmail.com>
2025-05-02 22:23:12 +0200

    Fibonacchi squares generator

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -16,11 +16,31 @@ function GetFibonacci(iteration)
         {
             fibonacci=data;
             console.log(fibonacci.sequenceArray); 
+            GenerateSquares(fibonacci.sequenceArray)
         })
         .catch(error => console.error('Error:', error));
 }
 
+function GenerateSquares(sequenceArray)
+{
+    let container = document.getElementById("container");
+    
+    let ratio = 300 / sequenceArray[sequenceArray.length-1]
+    for (let i = sequenceArray.length-1; i >= 0; i--) 
+    {
+        let square = document.createElement("canvas")
+        square.id = sequenceArray[i];
+        square.width=sequenceArray[i]*ratio;
+        square.height=sequenceArray[i]*ratio;
+        square.style.backgroundColor= "sandybrown";
+        square.style.border= "1px solid black";
 
+        container.appendChild(square)
+        
+    }
+    
+    console.log("Hozzáadva")
+}
 
 
 document.getElementById("sendButton").addEventListener("click", () => GetFibonacci(document.getElementById('fibonacciInput').value));
